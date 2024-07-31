5b0755dca3042a10030a944f5bebf934d0876ed7
Chris Laverdure <greghealburn@hotmail.com>
2024-07-31 17:07:10 -0600

    Introduce a new modern style for directory indexing, ditching the old KDE mock style.

--- src/DirectoryIndexing.cxx
+++ src/DirectoryIndexing.cxx
@@ -199,9 +199,6 @@ static std::string InsertIndexTable ( std::string *ArgVirtualPath, std::string A
         bool AlternatingVariable = false;
 		std::string Buffer;
         std::vector<DirectoryEntryStruct> Directoryvector;
-
-	// Top Table row.
-	Buffer += "<table id=\"DFS_table\">\n<tr class=\"DFS_headertablerow\"><th class=\"DFS_entrytype\">Entry Type</th><th class=\"DFS_entryname\">Entry Name</th><th class=\"DFS_entrysize\">Entry Size</th></tr>\n";
          
 	{
 		// Ensure we have a .. entry.
@@ -261,8 +258,8 @@ static std::string InsertIndexTable ( std::string *ArgVirtualPath, std::string A
 	{
 		// Resize it if we're about to run out of space.
 		if ( Buffer.capacity() - Buffer.size() < 100 )
-			Buffer.reserve(1000);
-            
+			Buffer.reserve(10000);
+     /*
 		// Alternate the CSS class for every other line.
 		if ( AlternatingVariable == true )
 		{
@@ -297,165 +294,25 @@ static std::string InsertIndexTable ( std::string *ArgVirtualPath, std::string A
 		if ( Directoryvector[Iterator].Folder )
 			Buffer += "<td class=\"DFS_entrysize\">&nbsp;</td></tr>\n";
 		else
-			Buffer += "<td class=\"DFS_entrysize\">" + std::string( ConvertSizeToFriendly( Directoryvector[Iterator].Size ) ) + "</td></tr>\n";
-	}
+			Buffer += "<td class=\"DFS_entrysize\">" + std::string( ConvertSizeToFriendly( Directoryvector[Iterator].Size ) ) + "</td></tr>\n";*/
 
-	// End the table.
-	Buffer += "</table>";
+		Buffer += "{ name: '" + Directoryvector[Iterator].Name + "'";
+
+		if (Directoryvector[Iterator].Folder)
+		{
+			Buffer += ", type : 'folder'";
+		}
+		else
+		{
+			Buffer += ", size : '" + std::string(ConvertSizeToFriendly(Directoryvector[Iterator].Size)) + "'";
+			Buffer += ", type : 'file'";
+		}
+
+		Buffer += ", src : '" + Directoryvector[Iterator].CompletePath + "' },\n";
+	}
 
 	// Spit the completed index table back into the fuction that called us.
 	return Buffer;
-}
-	
-static std::string ParseTemplate ( ifstream &ArgFile, std::string *ArgVirtualPath, std::string ArgPath )
-{
-	char *TempSpace;
-	int FileSize;
-	bool ActiveLoop = true;
-	std::string TemplateData;
-	std::string::size_type stringPosition;
-	std::string::size_type LowestMatch = 0;
-	set<std::string> FilesToHide;
-
-	// First determine the size of the file.
-        ArgFile.seekg(0, ios_base::end);
-        FileSize = static_cast<int>( ArgFile.tellg() );
-        ArgFile.seekg(0, ios_base::beg);
-        FileSize -= static_cast<int>( ArgFile.tellg() );
-
-	TempSpace = (char *) malloc( FileSize );
-	TempSpace[FileSize] = '\0';
-
-	// Read all the information from the file into this char.
-	ArgFile.read ( TempSpace, FileSize );
-
-	// Dump the char into a std::string, parse and replace all "keywords"
-
-	TemplateData = TempSpace;
-
-	free ( TempSpace );
-	
-	while (ActiveLoop)
-	{
-		std::string::size_type stringSize;
-		std::string::size_type LocalLowestMatch = 0;
-		int stringOffset = 0;
-
-		stringSize = TemplateData.size();
-		ActiveLoop = false;
-
-		// $$LOCATION$$
-		stringPosition = TemplateData.find ("$$LOCATION$$", LowestMatch);
-		if ( stringPosition != std::string::npos )
-		{
-			TemplateData.replace( stringPosition, sizeof("$$LOCATION$$") - 1, *ArgVirtualPath);
-
-			if ( stringPosition < LocalLowestMatch || LocalLowestMatch == 0 )
-				LocalLowestMatch = stringPosition;
-
-			stringOffset += ArgVirtualPath->size() - sizeof("$$LOCATION$$");
-			ActiveLoop = true;
-		}
-		// $$SERVERVERSION$$
-		stringPosition = TemplateData.find ("$$SERVERVERSION$$", LowestMatch);
-		if ( stringPosition != std::string::npos )
-		{
-			std::string Versionstring("DFileServer [Version " + std::to_string(Version::MAJORVERSION) + "." + std::to_string(Version::MINORVERSION) + "." + std::to_string(Version::PATCHVERSION) + "] -{" + Version::VERSIONTITLE + "}-");
-
-			TemplateData.replace( stringPosition, sizeof("$$SERVERVERSION$$") - 1, Versionstring );
-
-			if ( stringPosition < LocalLowestMatch || LocalLowestMatch == 0 )
-				LocalLowestMatch = stringPosition;
-
-			stringOffset += Versionstring.size() - sizeof("$$SERVERVERSION$$");
-			ActiveLoop = true;
-		}
-		// $$INSERTFILE(*)$$
-		stringPosition = TemplateData.find ("$$INSERTFILE(", LowestMatch);
-		if ( stringPosition != std::string::npos )
-		{
-			std::string FileData;
-			std::string::size_type stringEndPosition;
-
-			// Find where the end of this tag is...
-			stringEndPosition = TemplateData.find (")$$", stringPosition);
-
-			// Make sure the tag ends
-			if ( stringEndPosition != std::string::npos )
-			{
-				std::string TempFileName;
-				std::string::size_type TempPosition;
-
-				// Now we Seperate the chunck between these two positions.
-				TempPosition = stringPosition + sizeof("$$INSERTFILE");				
-				TempFileName = TemplateData.substr ( TempPosition, stringEndPosition - TempPosition );
-
-				// Make sure the filename doesn't contain certain elements that may be used
-				// to redirect the folder, and make sure that it's not a file that has already
-				// been included. ( Included files can have $$INSERTFILE tags, and they will be parsed.
-				// 		    If an inserted file inserts itself, it will recurse until the computer
-				//		    runs out of ram and the program dies. Not cool. )
-				if ( TempFileName.find("/", 0) == std::string::npos &&
-					TempFileName.find("\\", 0) == std::string::npos && 
-					TempFileName.find("~", 0) == std::string::npos &&
-					FilesToHide.find( TempFileName ) == FilesToHide.end() )
-				{ 
-					// Load the file into our std::string and add it to the FilesToHide std::vector
-					// so that it doesn't show up in our std::vector.
-					FileData = InsertFile( ArgPath.c_str(), TempFileName.c_str() );
-					FilesToHide.insert( TempFileName );
-				}
-				else
-					FileData.clear();
-
-				// We attempt to substitute $$INSERTFILE(*)$$ with the contents of FileData.
-				// This will simply remove the tag if FileData is blank.
-				TemplateData.replace( stringPosition, (stringEndPosition + 3) - stringPosition,
-							FileData );
-			}
-			else
-			{ // The tag is incomplete, cripple it so we don't scan it again.
-				TemplateData[stringPosition] = '^';
-				// Cancel out any offset detection.
-				stringPosition = ( stringEndPosition + 3 );
-			}
-
-			if ( stringPosition < LocalLowestMatch || LocalLowestMatch == 0 )
-				LocalLowestMatch = stringPosition;
-
-			stringOffset += FileData.size() - ( ( stringEndPosition + 3 ) - stringPosition );
-			ActiveLoop = true;
-		}
-
-		// Set the global lowest match to the local one.
-		// This is to save time on the searches.
-		LowestMatch = LocalLowestMatch;
-
-		// Check for a Negative offset.
-		if ( stringOffset < 0 )
-		{
-			std::string::size_type stringNewEnd;
-	
-			// Change it to a positive offset.
-			stringOffset *= -1;
-
-			// New end of the std::string.
-			stringNewEnd = stringSize - stringOffset;
-
-			// Zap the tail of the std::string, if it exists.
-			TemplateData.erase ( stringNewEnd, stringOffset );			
-		}
-
-	}
-
-	// This is a special case for the INDEX. We do it last and after the loop so that noting inside the index gets
-	// parsed and replaced. Not that I expect that to happen, but since when were such expectations safe?
-	stringPosition = TemplateData.find ("$$INDEXTABLE$$", 0);
-	if ( stringPosition != std::string::npos )
-		TemplateData.replace( stringPosition, sizeof("$$INDEXTABLE$$") - 1, 
-					InsertIndexTable( ArgVirtualPath, ArgPath, FilesToHide ) );
-
-	return TemplateData;
 }
 
 char GenerateFolderIndex( std::string ArgVirtualPath, char *ArgPath, std::string &ArgBuffer )
@@ -494,38 +351,154 @@ char GenerateFolderIndex( std::string ArgVirtualPath, char *ArgPath, std::string
 	}
 
 	if ( ArgBuffer.empty() )
-		ArgBuffer.reserve(1000);
+		ArgBuffer.reserve(10000);
 
-	// Toss in a custom template in if one exists. If not, just use a generic form.
-	FileStream.open ("indextemplates/default.html", ios::in ); // Open our template header.
-	if ( !FileStream.is_open() || !FileStream.good() )
-	{ // It doesn't exist, so toss in a generic header.
-		set<std::string> Filler;
+	set<std::string> Filler;
 
-		// <html> and <head>
-		ArgBuffer = "<html>\n<head>\n<title>Directory listing for " + std::string(ArgVirtualPath) + "</title>\n";
-		// A clone of KDE's public fileserver CSS sheet
-		ArgBuffer += "<style type=\"text/css\">\n<!--\n";
-		ArgBuffer += "body { font-family: Verdana, Arial, sans-serif;}\n#DFS_table {color: rgb(0, 0, 0);background-color: rgb(234, 233, 232); border: thin outset; width: 100%; }\n";
-		ArgBuffer += "#DFS_table td { margin: 0px; white-space: nowrap; }\n#DFS_table a { color: rgb(0, 0, 0); text-decoration: none; }\n";
-		ArgBuffer += "#DFS_table th { color: rgb(0, 0, 0); background-color: rgb(230, 240, 249); text-align: left; white-space: nowrap; border: thin outset; }\n";
-		ArgBuffer += "tr.DFS_eventablerow { background-color: rgb(255, 255, 255); color: rgb (0, 0, 0); }\n";
-		ArgBuffer += "tr.DFS_oddtablerow { background-color: rgb(238, 246, 255); color: rgb(0, 0, 0); }\n";
-		ArgBuffer += ".DFS_entrytype { text-align: center; width: 6em; }\ntd.DFS_entrytype { background-color: rgb(255, 255, 255); font-weight: bold; color: rgb(150, 150, 150);}\n";
-		ArgBuffer += ".DFS_entrysize { color: rgb(0, 0, 0); text-align: right;}\na.DFS_direntry { font-weight: bold; }\n";
-		ArgBuffer += "-->\n</style>\n";
-		// Insert the table.
-		ArgBuffer += InsertIndexTable( &ArgVirtualPath, std::string( ArgPath ), Filler );
-		// Server Version Information.
-		ArgBuffer += "<hr><i>DFileServer [Version " + std::to_string(Version::MAJORVERSION) + "." + std::to_string(Version::MINORVERSION) + "." + std::to_string(Version::PATCHVERSION) + "] -{" + Version::VERSIONTITLE + "}-</i>\n";
-		// End the index
-		ArgBuffer += "</body>\n</html>\n"; 
-	}
-	else
-	{ // Let's grab the one from the template and parse it.
-		ArgBuffer += ParseTemplate( FileStream, &ArgVirtualPath, std::string( ArgPath ) );
-		FileStream.close();
-	}
+	ArgBuffer = R"(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+          content="width=device-width, 
+                   initial-scale=1.0">
+    <title>)";
+	ArgBuffer += "DFileServer [" + std::to_string(Version::MAJORVERSION) + "." + std::to_string(Version::MINORVERSION) + "." + std::to_string(Version::PATCHVERSION) + "] " + ArgVirtualPath;
+	ArgBuffer += R"(</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+        .window {
+            width: 80%;
+            margin: 50px auto;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+        .window-header {
+            background-color: #0078d7;
+            color: #fff;
+            padding: 10px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .window-header .title {
+            font-size: 16px;
+        }
+        .window-header .buttons {
+            display: flex;
+            gap: 5px;
+        }
+        .window-header .buttons div {
+            width: 15px;
+            height: 15px;
+            background-color: #fff;
+            border-radius: 50%;
+            cursor: pointer;
+        }
+        .window-location {
+            padding: 10px;
+            background-color: #f0f0f0;
+            border-bottom: 1px solid #ccc;
+        }
+        .window-content {
+            padding: 20px;
+        }
+        .file-grid {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            gap: 10px;
+        }
+        .file-grid .file-item {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: center;
+            background-color: #fff;
+        }
+        .file-grid .file-item .file-name {
+            font-weight: bold;
+        }
+        .file-grid .file-item .file-size {
+            color: #666;
+        }
+        .file-grid .file-item.folder {
+            background-color: #e0e0e0;
+        }
+        .file-grid .file-item a {
+            text-decoration: none;
+            color: inherit;
+        }
+    </style>
+</head>
+<body>
+    <div class="window">
+        <div class="window-header">
+            <div class="title">)";
+	ArgBuffer += "DFileServer [" + std::to_string(Version::MAJORVERSION) + "." + std::to_string(Version::MINORVERSION) + "." + std::to_string(Version::PATCHVERSION) + "] " + ArgVirtualPath;
+	ArgBuffer += R"(
+	</div>
+            <div class="buttons">
+                <div style="background-color: #ff5f56;"></div>
+                <div style="background-color: #ffbd2e;"></div>
+                <div style="background-color: #27c93f;"></div>
+            </div>
+            <div class="version">)";
+	ArgBuffer += Version::VERSIONTITLE;
+	ArgBuffer += R"(
+            </div>
+        </div>
+        <div class="window-location">)";
+	ArgBuffer += ArgVirtualPath;
+	ArgBuffer += R"(	            
+        </div>
+        <div class="window-content">
+            <div class="file-grid" id="file-grid"></div>
+        </div>
+    </div>
+
+    <script>
+        const fileGrid = document.getElementById('file-grid');
+
+        // Example data
+        const items = [
+)";
+
+	ArgBuffer += InsertIndexTable(&ArgVirtualPath, std::string(ArgPath), Filler);
+
+	ArgBuffer += R"(
+        ];
+
+        items.forEach(item => {
+            const fileItem = document.createElement('div');
+            fileItem.className = 'file-item';
+            if (item.type === 'folder') {
+                fileItem.classList.add('folder');
+            }
+            const link = document.createElement('a');
+            link.href = item.src;
+            const fileName = document.createElement('div');
+            fileName.className = 'file-name';
+            fileName.textContent = item.name;
+            link.appendChild(fileName);
+            if (item.type === 'file') {
+                const fileSize = document.createElement('div');
+                fileSize.className = 'file-size';
+                fileSize.textContent = item.size;
+                link.appendChild(fileSize);
+            }
+            fileItem.appendChild(link);
+            fileGrid.appendChild(fileItem);
+        });
+    </script>
+</body>
+</html>
+)";
 
 	return 1;
 }
